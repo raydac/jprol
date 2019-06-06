177979ce18fd63d12fb4b9802e5a4deac8a6db9e
Igor Maznitsa <rrg4400@gmail.com>
2019-06-06 23:48:38 +0300

    refactoring

--- engine/jprol-jsr223/src/main/java/com/igormaznitsa/prol/script/JProlScriptContext.java
+++ engine/jprol-jsr223/src/main/java/com/igormaznitsa/prol/script/JProlScriptContext.java
@@ -25,110 +25,111 @@ import javax.script.ScriptContext;
 
 public final class JProlScriptContext implements ScriptContext {
 
-    private final ProlContext context;
-    private static JProlBindings globalMap = null;
-    private JProlBindings engineMap = new JProlBindings(this);
+  private final ProlContext context;
+  private static JProlBindings globalMap = null;
+  private JProlBindings engineMap = new JProlBindings(this);
 
-    private static final List<Integer> ALLOWED_SCOPES = Arrays.asList(ScriptContext.ENGINE_SCOPE, ScriptContext.GLOBAL_SCOPE);
+  private static final List<Integer> ALLOWED_SCOPES = Arrays.asList(ScriptContext.ENGINE_SCOPE, ScriptContext.GLOBAL_SCOPE);
 
-    JProlScriptContext(final String name) {
-        this.context = new ProlContext(name);
+  JProlScriptContext(final String name) {
+    this.context = new ProlContext(name);
+  }
+
+  public ProlContext getProlContext() {
+    return this.context;
+  }
+
+  @Override
+  public void setBindings(final Bindings bindings, final int scope) {
+    if (bindings == null) {
+      throw new NullPointerException("null bindings in ENGINE scope");
     }
-
-    public ProlContext getProlContext() {
-        return this.context;
+    switch (scope) {
+      case ScriptContext.ENGINE_SCOPE:
+        engineMap = (JProlBindings) bindings;
+        break;
+      case ScriptContext.GLOBAL_SCOPE:
+        globalMap = (JProlBindings) bindings;
+        break;
+      default:
+        throw new IllegalArgumentException("invalid scope");
     }
+  }
 
-    @Override
-    public void setBindings(final Bindings bindings, final int scope) {
-        if (bindings == null) {
-            throw new NullPointerException("null bindings in ENGINE scope");
-        }
-        switch (scope) {
-            case ScriptContext.ENGINE_SCOPE:
-                engineMap = (JProlBindings) bindings;
-                break;
-            case ScriptContext.GLOBAL_SCOPE:
-                globalMap = (JProlBindings) bindings;
-                break;
-            default:
-                throw new IllegalArgumentException("invalid scope");
-        }
+  JProlBindings getJprolBindings(final int scope) {
+    switch (scope) {
+      case ScriptContext.ENGINE_SCOPE:
+        return engineMap;
+      case ScriptContext.GLOBAL_SCOPE:
+        return globalMap;
+      default:
+        throw new IllegalArgumentException("invalid scope");
     }
+  }
 
-    public JProlBindings getJprolBindings(final int scope) {
-      switch (scope) {
-        case ScriptContext.ENGINE_SCOPE:
-          return engineMap;
-        case ScriptContext.GLOBAL_SCOPE:
-          return globalMap;
-        default:
-          throw new IllegalArgumentException("invalid scope");
-      }
-    }
-    
-    @Override
-    public Bindings getBindings(final int scope) {
-      return this.getJprolBindings(scope);
-    }
+  @Override
+  public Bindings getBindings(final int scope) {
+    return this.getJprolBindings(scope);
+  }
 
-    @Override
-    public void setAttribute(String name, Object value, int scope) {
-    }
+  @Override
+  public void setAttribute(final String name, final Object value, final int scope) {
+    this.getJprolBindings(scope).put(name, value);
+  }
 
-    @Override
-    public Object getAttribute(String name, int scope) {
-        return null;
-    }
+  @Override
+  public Object getAttribute(final String name, final int scope) {
+    return this.getJprolBindings(scope).get(name);
+  }
 
-    @Override
-    public Object removeAttribute(String name, int scope) {
-        return null;
-    }
+  @Override
+  public Object removeAttribute(final String name, final int scope) {
+    return this.getJprolBindings(scope).remove(name);
+  }
 
-    @Override
-    public Object getAttribute(String name) {
-        return null;
-    }
+  @Override
+  public Object getAttribute(String name) {
+    return ALLOWED_SCOPES.stream().map(x -> this.getAttribute(name, x)).filter(x -> x != null).findFirst().orElse(null);
+  }
 
-    @Override
-    public int getAttributesScope(String name) {
-        return -1;
-    }
+  @Override
+  public int getAttributesScope(final String name) {
+    return ALLOWED_SCOPES.stream().filter(x -> this.getJprolBindings(x).containsKey(name)).findFirst().orElse(-1);
+  }
 
-    @Override
-    public Writer getWriter() {
-        return null;
-    }
+  @Override
+  public Writer getWriter() {
+    return null;
+  }
 
-    @Override
-    public Writer getErrorWriter() {
-        return null;
-    }
+  @Override
+  public Writer getErrorWriter() {
+    return null;
+  }
 
-    @Override
-    public void setWriter(Writer writer) {
+  @Override
+  public void setWriter(Writer writer) {
 
-    }
+  }
 
-    @Override
-    public void setErrorWriter(Writer writer) {
+  @Override
+  public void setErrorWriter(Writer writer) {
 
-    }
+  }
 
-    @Override
-    public Reader getReader() {
-        return null;
-    }
+  @Override
+  public Reader getReader() {
+    return null;
+  }
 
-    @Override
-    public void setReader(Reader reader) {
+  @Override
+  public void setReader(Reader reader) {
 
-    }
+  }
 
-    @Override
-    public List<Integer> getScopes() {
-        return ALLOWED_SCOPES;
-    }
+  @Override
+  public List<Integer> getScopes() {
+    return ALLOWED_SCOPES;
+  }
 
 }
