64b6b3266c54e6bb7f4f264b0657d922179f684c
Igor Maznitsa <rrg4400@gmail.com>
2025-03-05 10:27:02 +0200

    refactoring

---
+++ engine/jprol-core/src/main/java/com/igormaznitsa/jprol/utils/JProlExternalServiceUtils.java
@@ -0,0 +1,34 @@
+package com.igormaznitsa.jprol.utils;
+
+import com.igormaznitsa.jprol.libs.extservice.JProlExternalServiceFactory;
+import java.util.List;
+import java.util.ServiceLoader;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Utility methods to work with external services.
+ *
+ * @since 2.2.2
+ */
+public final class JProlExternalServiceUtils {
+  private JProlExternalServiceUtils() {
+
+  }
+
+  /**
+   * Find all JProl external service factories,
+   *
+   * @param classLoaders additional class loaders to search factories.
+   * @return list of all factories in visibility.
+   */
+  public static List<JProlExternalServiceFactory> findAllExternalServiceFactories(
+      final List<ClassLoader> classLoaders) {
+    return Stream.concat(ServiceLoader.load(JProlExternalServiceFactory.class).stream(),
+            classLoaders.stream()
+                .flatMap(x -> ServiceLoader.load(JProlExternalServiceFactory.class, x).stream()))
+        .map(ServiceLoader.Provider::get)
+        .collect(Collectors.toList());
+  }
+
+}
