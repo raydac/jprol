c2109164cc40a0ce926d550f20844360e7a388a5
Igor Maznitsa <rrg4400@gmail.com>
2025-12-28 23:36:43 +0200

    added test

---
+++ engine/jprol-core/src/test/java/com/igormaznitsa/jprol/data/TermStructTest.java
@@ -0,0 +1,32 @@
+package com.igormaznitsa.jprol.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.igormaznitsa.jprol.it.AbstractJProlTest;
+import com.igormaznitsa.jprol.logic.JProlChoicePoint;
+import com.igormaznitsa.jprol.logic.JProlContext;
+import java.io.StringReader;
+import org.junit.jupiter.api.Test;
+
+class TermStructTest extends AbstractJProlTest {
+
+  @Test
+  void testTransferPayload() {
+    try (final JProlContext context = this.makeTestContext()) {
+      context.consult(new StringReader(
+          "c(A,Y) :- s(_,_,Y) = A. b([X],Y) :- Z = [X], [L] = Z, c(s(1,2,L),Y).  a(X,Y) :- b([X],Z), Z = Y."));
+      final Object payload = new Object();
+      final Term valX = Terms.newAtom("ABC", payload);
+      final TermVar result = Terms.newVar("Y");
+      final TermStruct goal = Terms.newStruct(Terms.newAtom("a"), valX, result);
+
+      final JProlChoicePoint choicePoint = new JProlChoicePoint(goal, context);
+
+      assertNotNull(choicePoint.prove());
+
+      assertEquals("ABC", result.tryGround().getText());
+      assertEquals(payload, result.tryGround().getPayload());
+    }
+  }
+}
